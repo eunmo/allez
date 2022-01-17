68c9daae2df99b117e29d20aec68ae2c68a61244
eunmo <eunmo@cs.stanford.edu>
2022-01-17 15:43:00 +0000

    game router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
+const game = require('./game');
 const person = require('./person');
 
 const router = express.Router();
+router.use('/game', game);
 router.use('/person', person);
 
 module.exports = router;
--- server/routes/query.test.js
+++ server/routes/query.test.js
@@ -29,3 +29,48 @@ test('get all persons', async () => {
   const body = await get('/api/person/list');
   expect(body.length).toBe(3);
 });
+
+test('get game dates', async () => {
+  const body = await get('/api/game/dates');
+  expect(body.length).toBe(2);
+  expect(body.map((e) => new Date(e.date))).toStrictEqual([date2, date1]);
+});
+
+test('get games by date', async () => {
+  async function check(date, gids) {
+    const dateString = date.toISOString().substring(0, 10);
+    const body = await get(`/api/game/date/${dateString}`);
+    expect(body.length).toBe(gids.length);
+    expect(body.map((g) => g.id)).toStrictEqual(gids);
+  }
+
+  await check(date1, [gid1]);
+  await check(date2, [gid3, gid2]);
+});
+
+test('get games by pid', async () => {
+  async function check(pid, gids) {
+    const body = await get(`/api/game/person/${pid}`);
+    expect(body.length).toBe(gids.length);
+    expect(body.map((g) => g.id)).toStrictEqual(gids);
+  }
+
+  await check(pid1, [gid3, gid1]);
+  await check(pid2, [gid3, gid2, gid1]);
+  await check(pid3, [gid3, gid2]);
+});
+
+test('get history', async () => {
+  async function check(pid1, pid2, gids) {
+    const body = await get(`/api/game/history/${pid1}/${pid2}`);
+    expect(body.length).toBe(gids.length);
+    expect(body.map((g) => g.id)).toStrictEqual(gids);
+  }
+
+  await check(pid1, pid2, [gid3, gid1]);
+  await check(pid1, pid3, [gid3]);
+  await check(pid2, pid1, [gid3, gid1]);
+  await check(pid2, pid3, [gid3, gid2]);
+  await check(pid3, pid1, [gid3]);
+  await check(pid3, pid2, [gid3, gid2]);
+});
