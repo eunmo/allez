4faacbd6585b244496b3f92acb660177f20f8678
eunmo <eunmo@cs.stanford.edu>
2022-01-19 07:03:46 +0000

    fix build issue (of mini-css-extract-plugin)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import style from './App.module.css';
 
 function App() {
-  return <div className={style.App}>Hello!</div>;
+  return (
+    <div className={style.App}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<>Hello</>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
