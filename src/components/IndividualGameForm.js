d9b8b41700eaf938dd3f910ca2744e3d2d6d79d7
eunmo <eunmo@cs.stanford.edu>
2022-02-12 12:39:44 +0000

    PointInput component

--- src/components/IndividualGameForm.js
+++ src/components/IndividualGameForm.js
@@ -2,60 +2,8 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { get, toPersonIdMap } from '../utils';
 import PersonSelect from './PersonSelect';
+import PointInput from './PointInput';
 import style from './IndividualGameForm.module.css';
-
-function PointInputValue({ digit, onClick, disabled }) {
-  return (
-    <input
-      type="button"
-      value={digit}
-      onClick={(e) => onClick(e.target.value)}
-      disabled={disabled}
-    />
-  );
-}
-
-function parseValue(value) {
-  return parseInt(value ?? '0', 10);
-}
-
-function PointInput({ value, setValue, onDone }) {
-  const onClickDigit = useCallback(
-    (digit) => {
-      setValue(`${parseValue(value) * 10 + parseValue(digit)}`);
-      onDone();
-    },
-    [value, setValue, onDone]
-  );
-
-  const backspace = useCallback(() => {
-    if (!value) {
-      return;
-    }
-
-    if (value.length === 1) {
-      setValue();
-    } else {
-      setValue(value.substring(0, value.length - 1));
-    }
-  }, [value, setValue]);
-
-  return (
-    <div className={style.PointInput}>
-      {[7, 8, 9, 4, 5, 6, 1, 2, 3].map((d) => (
-        <PointInputValue key={d} digit={d} onClick={onClickDigit} />
-      ))}
-      <input
-        type="button"
-        value="C"
-        onClick={() => setValue()}
-        disabled={!value}
-      />
-      <PointInputValue digit={0} onClick={onClickDigit} />
-      <input type="button" value="⌫" onClick={backspace} disabled={!value} />
-    </div>
-  );
-}
 
 function getInputClass(value, target, selected) {
   if (target === selected) {
---
+++ src/components/PointInput.js
@@ -0,0 +1,56 @@
+import { useCallback } from 'react';
+
+import style from './PointInput.module.css';
+
+function PointInputValue({ digit, onClick, disabled }) {
+  return (
+    <input
+      type="button"
+      value={digit}
+      onClick={(e) => onClick(e.target.value)}
+      disabled={disabled}
+    />
+  );
+}
+
+function parseValue(value) {
+  return parseInt(value ?? '0', 10);
+}
+
+export default function PointInput({ value, setValue, onDone }) {
+  const onClickDigit = useCallback(
+    (digit) => {
+      setValue(`${parseValue(value) * 10 + parseValue(digit)}`);
+      onDone();
+    },
+    [value, setValue, onDone]
+  );
+
+  const backspace = useCallback(() => {
+    if (!value) {
+      return;
+    }
+
+    if (value.length === 1) {
+      setValue();
+    } else {
+      setValue(value.substring(0, value.length - 1));
+    }
+  }, [value, setValue]);
+
+  return (
+    <div className={style.PointInput}>
+      {[7, 8, 9, 4, 5, 6, 1, 2, 3].map((d) => (
+        <PointInputValue key={d} digit={d} onClick={onClickDigit} />
+      ))}
+      <input
+        type="button"
+        value="C"
+        onClick={() => setValue()}
+        disabled={!value}
+      />
+      <PointInputValue digit={0} onClick={onClickDigit} />
+      <input type="button" value="⌫" onClick={backspace} disabled={!value} />
+    </div>
+  );
+}
