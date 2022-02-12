cbe6abab704b7a4bebcb02a57138726d47be6c24
eunmo <eunmo@cs.stanford.edu>
2022-02-12 17:02:28 +0000

    lint

--- src/AddTeamGame.js
+++ src/AddTeamGame.js
@@ -78,9 +78,7 @@ function ChooseTeamMembers({
         ['팀 2', rs],
       ].map(([teamName, list], teamIndex) => (
         <Fragment key={teamName}>
-          <label className={style.label}>
-            {teamName}
-          </label>
+          <label className={style.label}>{teamName}</label>
           {list.map((id, index) => (
             <input
               /* eslint-disable-next-line react/no-array-index-key */
