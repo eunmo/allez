5857fac027edb324a9ba441a93e99d6868ca2fe8
eunmo <eunmo@cs.stanford.edu>
2022-02-22 15:31:00 +0000

    update Main Menu layout

--- src/Main.js
+++ src/Main.js
@@ -5,24 +5,11 @@ import { GameList, LinkButton } from './components';
 import { get } from './utils';
 import style from './Main.module.css';
 
-function Menu() {
-  return (
-    <>
-      <LinkButton size="lg" to="/game/individual/add">
-        개인전 기록
-      </LinkButton>
-      <LinkButton to="/game/team/add">단체전 시작</LinkButton>
-      <LinkButton to="/game/calendar">과거 기록 열람</LinkButton>
-      <LinkButton to="/person">출석 체크</LinkButton>
-    </>
-  );
-}
-
 function getTime() {
   const now = new Date();
   const hour = now.getHours() % 12;
   const minute = now.getMinutes();
-  return [hour, minute].map((n) => `${n}`.padStart(2, '\xa0')).join(':');
+  return [hour, minute].map((n) => `${n}`.padStart(2, '0')).join(':');
 }
 
 function Clock() {
@@ -33,7 +20,29 @@ function Clock() {
     return () => clearInterval(timerId);
   }, []);
 
-  return <div className={style.Clock}>{time}</div>;
+  return <div className={`${style.Clock} ${style.span3}`}>{time}</div>;
+}
+
+function Menu() {
+  const isLarge = useMediaQuery({ query: '(min-width: 601px)' });
+
+  return (
+    <div className={style.Menu}>
+      <LinkButton size="lg" to="/game/individual/add" cn={style.span3}>
+        개인전 기록
+      </LinkButton>
+      {isLarge && <Clock />}
+      <LinkButton to="/game/team/add" cn={style.span2}>
+        단체전 시작
+      </LinkButton>
+      <LinkButton to="/game/calendar" cn={style.span2}>
+        과거 기록 열람
+      </LinkButton>
+      <LinkButton to="/person" cn={style.span2}>
+        출석 체크
+      </LinkButton>
+    </div>
+  );
 }
 
 export default function Main() {
@@ -49,9 +58,17 @@ export default function Main() {
     return <Menu />;
   }
 
+  if (isLarge) {
+    return (
+      <>
+        <Menu />
+        <GameList games={games} today />
+      </>
+    );
+  }
+
   return (
     <GameList games={games} today>
-      {isLarge && <Clock />}
       <Menu />
     </GameList>
   );
