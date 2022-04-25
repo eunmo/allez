45c7c40374b87ffe0c10e8fabaebca52e74d02a0
eunmo <eunmo@cs.stanford.edu>
2022-04-25 15:12:16 +0000

    wide column for person select (only if > 6 in group)

--- src/components/PersonSelect.js
+++ src/components/PersonSelect.js
@@ -4,17 +4,30 @@ import { groupByPersonTypeName } from '../utils';
 import style from './PersonSelect.module.css';
 
 export default function PersonSelect({ persons, onClick, cn, alignRight }) {
-  const sections = useMemo(() => groupByPersonTypeName(persons), [persons]);
-
-  const firstStyle = {};
-  if (alignRight && sections.length < 3) {
-    firstStyle.gridColumnStart = 4 - sections.length;
-  }
+  const [sections, widthSum] = useMemo(() => {
+    const grouped = groupByPersonTypeName(persons);
+    const widths = grouped.map(({ persons: ps }) => (ps.length > 6 ? 2 : 1));
+    const sum = widths.reduce((a, b) => a + b, 0);
+    const cumSum = [1];
+    if (alignRight && sum < 3) {
+      cumSum[0] = 4 - sum;
+    }
+    widths.forEach((width, index) => cumSum.push(cumSum[index] + width));
+    const fullSections = grouped.map((section, i) => {
+      const st = { gridColumn: `${cumSum[i]} / ${cumSum[i + 1]}` };
+      const cname = widths[i] === 2 ? style.wideColumn : '';
+      return { ...section, st, cname };
+    });
+    return [fullSections, Math.max(sum, 3)];
+  }, [persons, alignRight]);
 
   return (
-    <div className={`${style.PersonSelect} ${cn}`}>
-      {sections.map(({ name, persons: personsInSection }, index) => (
-        <div key={name} style={index === 0 ? firstStyle : {}}>
+    <div
+      className={`${style.PersonSelect} ${cn}`}
+      style={{ gridTemplateColumns: `repeat(${widthSum}, 1fr` }}
+    >
+      {sections.map(({ name, persons: personsInSection, st, cname }) => (
+        <div key={name} className={cname} style={st}>
           <label className={style.label}>{name}</label>
           {personsInSection.map(({ firstName, id }) => (
             <input
