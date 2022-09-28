240c9325df7995207538095c85897183d5d926cd
eunmo <eunmo@cs.stanford.edu>
2022-09-28 16:09:30 +0000

    remove tournaments from global ranking for now

--- server/routes/rank.js
+++ server/routes/rank.js
@@ -5,51 +5,54 @@ const router = express.Router();
 
 function calculateRanks(games) {
   const personMap = new Map();
-  games.forEach(({ time, ls, rs, rounds }) => {
-    const date = time.toISOString().substring(0, 10);
-    [...ls, ...rs].forEach((pid) => {
-      if (!personMap.has(pid)) {
-        personMap.set(pid, {
-          lastDate: date,
-          date: 1,
-          wins: 0,
-          points: 0,
-          diff: 0,
+  games
+    // TODO handle tournaments
+    .filter(({ type }) => type !== 'T')
+    .forEach(({ time, ls, rs, rounds }) => {
+      const date = time.toISOString().substring(0, 10);
+      [...ls, ...rs].forEach((pid) => {
+        if (!personMap.has(pid)) {
+          personMap.set(pid, {
+            lastDate: date,
+            date: 1,
+            wins: 0,
+            points: 0,
+            diff: 0,
+          });
+        }
+
+        const person = personMap.get(pid);
+        if (person.lastDate !== date) {
+          person.lastDate = date;
+          person.date += 1;
+        }
+      });
+
+      const lastIndex = rounds.length - 1;
+      const { lp: lastLp, rp: lastRp } = rounds[lastIndex] ?? { lp: 0, rp: 0 };
+      if (lastLp > lastRp) {
+        ls.forEach((pid) => {
+          personMap.get(pid).wins += 1;
+        });
+      }
+      if (lastLp < lastRp) {
+        rs.forEach((pid) => {
+          personMap.get(pid).wins += 1;
         });
       }
 
-      const person = personMap.get(pid);
-      if (person.lastDate !== date) {
-        person.lastDate = date;
-        person.date += 1;
-      }
-    });
-
-    const lastIndex = rounds.length - 1;
-    const { lp: lastLp, rp: lastRp } = rounds[lastIndex] ?? { lp: 0, rp: 0 };
-    if (lastLp > lastRp) {
-      ls.forEach((pid) => {
-        personMap.get(pid).wins += 1;
+      let [prevLp, prevRp] = [0, 0];
+      rounds.forEach(({ l, r, lp: curLp, rp: curRp }) => {
+        const [lp, rp] = [curLp - prevLp, curRp - prevRp];
+        [prevLp, prevRp] = [curLp, curRp];
+        const left = personMap.get(l);
+        left.points += lp;
+        left.diff += lp - rp;
+        const right = personMap.get(r);
+        right.points += rp;
+        right.diff += rp - lp;
       });
-    }
-    if (lastLp < lastRp) {
-      rs.forEach((pid) => {
-        personMap.get(pid).wins += 1;
-      });
-    }
-
-    let [prevLp, prevRp] = [0, 0];
-    rounds.forEach(({ l, r, lp: curLp, rp: curRp }) => {
-      const [lp, rp] = [curLp - prevLp, curRp - prevRp];
-      [prevLp, prevRp] = [curLp, curRp];
-      const left = personMap.get(l);
-      left.points += lp;
-      left.diff += lp - rp;
-      const right = personMap.get(r);
-      right.points += rp;
-      right.diff += rp - lp;
     });
-  });
 
   const ranks = [...personMap.entries()].map(
     ([id, { date, wins, points, diff }]) => ({ id, date, wins, points, diff })
